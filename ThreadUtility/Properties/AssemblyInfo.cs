dfae1874f212f2a99b56a13b0ae5040be7cfb1af
superbr4in <15275491+superbr4in@users.noreply.github.com>
2018-09-11 17:10:10 +0200

    Created 'ThreadUtility' VS project.

---
+++ ThreadUtility/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCopyright("Copyright © Elias Brandstetter 2018")]
+[assembly: AssemblyProduct("ThreadUtility")]
+[assembly: AssemblyTitle("ThreadUtility")]
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: ComVisible(false)]
+[assembly: Guid("9e915b24-dfe0-448a-8a31-9882cae21568")]
